d4cac878f1506d5150d727f846b5d0c16f41b274
Ricardo Avelar <ricardodiasmode@hotmail.com>
2022-05-21 14:24:26 -0300

    opencv implemented

--- Source/CRAZY_HEAD_DANCE/CRAZY_HEAD_DANCE.Build.cs
+++ Source/CRAZY_HEAD_DANCE/CRAZY_HEAD_DANCE.Build.cs
@@ -1,23 +1,76 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class CRAZY_HEAD_DANCE : ModuleRules
 {
+	private string ThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
+	}
+
 	public CRAZY_HEAD_DANCE(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        // PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+
+        LoadOpenCV(Target);
+    }
+
+    public bool LoadOpenCV(ReadOnlyTargetRules Target)
+    {
+        // Start OpenCV linking here!
+        bool isLibrarySupported = false;
+
+        // Create OpenCV Path 
+        string OpenCVPath = Path.Combine(ThirdPartyPath, "OpenCV");
+
+        // Get Library Path 
+        string LibPath = "";
+        bool isdebug = Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            LibPath = Path.Combine(OpenCVPath, "Libraries", "Win64");
+            isLibrarySupported = true;
+        }
+        else
+        {
+            string Err = string.Format("{0} dedicated server is made to depend on {1}. We want to avoid this, please correct module dependencies.", Target.Platform.ToString(), this.ToString()); System.Console.WriteLine(Err);
+        }
+
+        if (isLibrarySupported)
+        {
+            //Add Include path 
+            PublicIncludePaths.AddRange(new string[] { Path.Combine(OpenCVPath, "Includes") });
+
+            // Add Library Path 
+            PublicSystemLibraryPaths.Add(LibPath);
+
+            //Add Static Libraries
+            PublicSystemLibraryPaths.Add("opencv_world320.lib");
+
+            //Add Dynamic Libraries
+            PublicDelayLoadDLLs.Add("opencv_world320.dll");
+            PublicDelayLoadDLLs.Add("opencv_ffmpeg320_64.dll");
+        }
+
+        PublicDefinitions.Add(string.Format("WITH_OPENCV_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
+
 }
