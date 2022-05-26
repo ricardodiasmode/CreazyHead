c83160f6732078f4877976de617d0bca549cda6e
Ricardo Avelar <ricardodiasmode@hotmail.com>
2022-05-26 20:40:40 -0300

    face detection done

--- Source/CRAZY_HEAD_DANCE/FaceDetector.cpp
+++ Source/CRAZY_HEAD_DANCE/FaceDetector.cpp
@@ -27,7 +27,7 @@ void AFaceDetector::BeginPlay()
             GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("DB Faces loaded!"));
             UE_LOG(LogTemp, Warning, TEXT("DB Faces loaded!"));
             FTimerHandle UnusedTimer;
-            GetWorldTimerManager().SetTimer(UnusedTimer, this, &AFaceDetector::DetectAndDraw, 0.05f, true, 0.f);
+            GetWorldTimerManager().SetTimer(UnusedTimer, this, &AFaceDetector::DetectAndDraw, 0.05f, true, 2.f);
         }
     }
 }
@@ -43,30 +43,53 @@ void AFaceDetector::DetectAndDraw()
     // Capture frames from video and detect faces
     Vcapture >> frame;
         
-    // Whether or not captured something
-    if (frame.empty())
-        return;
-    cv::Mat img = frame.clone();
+    AuxFaceDetector face_detector;
 
-    // Whether or not there is a image
-    if (img.empty())
-        return;
+    auto rectangles = face_detector.detect_face_rectangles(frame);
+    cv::Scalar color(0, 105, 205);
+    int frame_thickness = 4;
+    for (const auto& r : rectangles) 
+        cv::rectangle(frame, r, color, frame_thickness);
+    imshow("Image", frame);
+}
+
+AuxFaceDetector::AuxFaceDetector() : confidence_threshold_(0.5), input_image_height_(300), input_image_width_(300),
+scale_factor_(1.0), mean_values_({ 104., 177.0, 123.0 }) 
+{
+
+    network_ = cv::dnn::readNetFromCaffe("C:/Program Files/Epic Games/UnrealProjects/CreazyHead/ThirdParty/OpenCV/Includes/opencvAssets/deploy.prototxt", "C:/Program Files/Epic Games/UnrealProjects/CreazyHead/ThirdParty/OpenCV/Includes/opencvAssets/res10_300x300_ssd_iter_140000_fp16.caffemodel");
+
+    if (network_.empty()) {
+        std::ostringstream ss;
+        ss << "Failed to load network with the following settings:\n"
+            << "Configuration: " + std::string("C:/Program Files/Epic Games/UnrealProjects/CreazyHead/ThirdParty/OpenCV/Includes/opencvAssets/deploy.prototxt") + "\n"
+            << "Binary: " + std::string("C:/Program Files/Epic Games/UnrealProjects/CreazyHead/ThirdParty/OpenCV/Includes/opencvAssets/res10_300x300_ssd_iter_140000_fp16.caffemodel") + "\n";
+        throw std::invalid_argument(ss.str());
+    }
+}
+
+std::vector<cv::Rect> AuxFaceDetector::detect_face_rectangles(const cv::Mat& frame) {
+    cv::Mat input_blob = cv::dnn::blobFromImage(frame, scale_factor_, cv::Size(input_image_width_, input_image_height_),
+        mean_values_, false, false);
+    network_.setInput(input_blob, "data");
+    cv::Mat detection = network_.forward("detection_out");
+    cv::Mat detection_matrix(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
 
     std::vector<cv::Rect> faces;
 
-    cascade.detectMultiScale(img, faces);
-    imshow("Image", img);
+    for (int i = 0; i < detection_matrix.rows; i++) {
+        float confidence = detection_matrix.at<float>(i, 2);
 
-    // Whether or not there is a face
-    if(faces.size() == 0)
-        return;
+        if (confidence < confidence_threshold_) {
+            continue;
+        }
+        int x_left_bottom = static_cast<int>(detection_matrix.at<float>(i, 3) * frame.cols);
+        int y_left_bottom = static_cast<int>(detection_matrix.at<float>(i, 4) * frame.rows);
+        int x_right_top = static_cast<int>(detection_matrix.at<float>(i, 5) * frame.cols);
+        int y_right_top = static_cast<int>(detection_matrix.at<float>(i, 6) * frame.rows);
 
-    UE_LOG(LogTemp, Warning, TEXT("%d"), faces.size());
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        int x1 = faces[i].x;
-        int y1 = faces[i].y;
-        int x2 = faces[i].x + faces[i].width;
-        int y2 = faces[i].y + faces[i].height;
+        faces.emplace_back(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));
     }
+
+    return faces;
 }
--- Source/CRAZY_HEAD_DANCE/FaceDetector.h
+++ Source/CRAZY_HEAD_DANCE/FaceDetector.h
@@ -7,6 +7,8 @@
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/dnn/dnn.hpp"
+#include "opencv2/opencv.hpp"
 #include "FaceDetector.generated.h"
 
 UCLASS()
@@ -35,5 +37,30 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+};
+
+class AuxFaceDetector {
+public:
+    explicit AuxFaceDetector();
+
+    /// Detect faces in an image frame
+    /// \param frame Image to detect faces in
+    /// \return Vector of detected faces
+    std::vector<cv::Rect> detect_face_rectangles(const cv::Mat& frame);
+
+private:
+    /// Face detection network
+    cv::dnn::Net network_;
+    /// Input image width
+    const int input_image_width_;
+    /// Input image height
+    const int input_image_height_;
+    /// Scale factor when creating image blob
+    const double scale_factor_;
+    /// Mean normalization values network was trained with
+    const cv::Scalar mean_values_;
+    /// Face detection confidence threshold
+    const float confidence_threshold_;
 
 };
